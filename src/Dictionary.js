5f574a9b09c64c7287ad6f409ca727300663c89b
nadPle <143026099+nadPle@users.noreply.github.com>
2024-04-04 20:51:59 +0200

    Added dictionary api integration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,21 @@
 import React, { useState } from "react";
 import "./Dictionary.css";
+import axios from "axios";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
 
+  function handleResponse(response) {
+    alert(response.data.meanings[0].definition);
+  }
+
   function search(event) {
     event.preventDefault();
-    alert(keyword);
+
+    let apiKey = "491037f95bt62c7eo1c6b568c53adb94";
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleKeywordChange(event) {
